b21befecb021d9ae0deee7b0403b1d3a5a540d12
RichardT74 <richtrist@gmail.com>
2019-07-20 14:43:05 +0100

    about to create seeds

--- models/animal.rb
+++ models/animal.rb
@@ -4,12 +4,36 @@ require_relative('.owner.rb')
 class Animal
 
 	attr_reader :id
-	attr_accessor :name, :type, :breed, :age, :admission_date, :adoption :owner_id
+	attr_accessor :name, :type, :breed, :age, :admission_date, :ready_to_adopt :owner_id
 
 	def initinalize ( options )
 		@id = options['id'].to_i
 		@name = options['name']
 		@type = options['type']
 		@breed = options['breed']
-		@age = option['age']
-		
+		@age = options['age'].to_i
+		@admission_date = options['admission_date']
+		@ready_to_adopt = options['ready_to_adopt'] ||=false
+		@owner_id = options['owner_id']
+	end
+
+	def save
+		sql = "INSERT INTO animals
+		(
+			name,
+			type,
+			breed,
+			age,
+			admission_date,
+			ready_to_adopt,
+			owner_id
+			)
+			VALUES
+			(
+				$1, $2, $3, $4, $5, $6, $7
+			)
+			RETURNING *"
+			values = [@name, @type, @breed, @age, @admission_date, @ready_to_adopt, @owner_id]
+			animal_data = SqlRuner.run(sql, values)
+			@id = student_data.first()['id'].to_i
+		end
