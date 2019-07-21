c6152eb8085a586df77e74e70445d069f299130f
RichardT74 <richtrist@gmail.com>
2019-07-21 12:25:36 +0100

    new animals deleted db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,17 @@ owner1 = Owner.new({
 	})
 owner1.save()
 
+owner2 = Owner.new({
+	"owner_name" => "John",
+	})
+owner2.save()
+
+
+owner3 = Owner.new({
+	"owner_name" => "Frank",
+	})
+owner3.save()
+
 
 animal1 = Animal.new({
   "name" => "rex",
@@ -28,4 +39,27 @@ animal1 = Animal.new({
 		"ready_to_adopt" => true,
 		"owner_id" => owner1.id
 		})
+		animal2.save()
+
+
+		animal3 = Animal.new({
+			"name" => "Mr Tibs",
+			"type" => "cat",
+			"breed" => "British Shorthair",
+			"age" => 1,
+			"admission_date" => "02/02/2019",
+			"ready_to_adopt" => true,
+			"owner_id" => owner2.id
+			})
+			animal2.save()
+
+		animal3 = Animal.new({
+				"name" => "Fergus",
+				"type" => "cat",
+				"breed" => "Maine Coon",
+				"age" => 1,
+				"admission_date" => "01/01/2019",
+				"ready_to_adopt" => true,
+				"owner_id" => owner2.id
+				})
 		animal2.save()
